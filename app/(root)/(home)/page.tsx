aa83841756cabaae2077abfb53702513e93cc9d5
Emin Suleymanov <emin.suleymanov2001@gmail.com>
2024-05-05 23:28:31 +0400

    complate

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,9 +2,8 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now = new Date();
-
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const time = now.toLocaleTimeString('az-AZ', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('az-AZ', { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
